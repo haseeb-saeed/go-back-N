f7b0bf125dff71fc12aea5ceb055664a9c42ab93
haseeb-saeed <hs17@hotmail.co.uk>
2016-07-18 20:35:01 -0400

    Turns out I can't spell unreliable

--- udt.py
+++ udt.py
@@ -1,17 +1,17 @@
-# udt.py - Unrealiable data transfer using UDP
+# udt.py - Unreliable data transfer using UDP
 import random
 import socket
 
 DROP_PROB = 4
 
-# Send a packet across the unrealiable channel
+# Send a packet across the unreliable channel
 # Packet may be lost
 def send(packet, sock, addr):
     if random.randint(0, DROP_PROB) > 0:
         sock.sendto(packet, addr)
     return
 
-# Receive a packet from the unrealiable channel
+# Receive a packet from the unreliable channel
 def recv(sock):
     data, addr = sock.recvfrom(512)
     packet = data.decode('utf-8')
