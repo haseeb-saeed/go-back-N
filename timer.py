97d9c2bd82e9fa4fe74e2d2ce31af4696a828e27
Haseeb Saeed <hs17@hotmail.co.uk>
2016-07-21 16:33:02 -0400

    Make timer great again

---
+++ timer.py
@@ -0,0 +1,30 @@
+import time
+
+class Timer(object):
+    TIMER_STOP = -1
+
+    def __init__(self, duration):
+        self._start_time = self.TIMER_STOP
+        self._duration = duration
+
+    # Starts the timer
+    def start(self):
+        if self._start_time == self.TIMER_STOP:
+            self._start_time = time.time()
+
+    # Stops the timer
+    def stop(self):
+        if self._start_time != self.TIMER_STOP:
+            self._start_time = self.TIMER_STOP
+
+    # Determines whether the timer is runnning
+    def running(self):
+        return self._start_time != self.TIMER_STOP
+
+    # Determines whether the timer timed out
+    def timeout(self):
+        if not self.running():
+            return False
+        else:
+            return time.time() - self._start_time >= self._duration
+    
