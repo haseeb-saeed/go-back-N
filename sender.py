916909c026142067631ab0b40bca64adba78eca4
Haseeb Saeed <hs17@hotmail.co.uk>
2016-07-21 15:02:44 -0400

    Some basic sender work

--- sender.py
+++ sender.py
@@ -3,38 +3,67 @@ import socket
 import _thread
 import udt
 
-ACK_HEADER = 'A'
-MSG_HEADER = 'M'
+PACKET_SIZE = 512
 RECEIVER_IP = 'localhost'
 RECEIVER_PORT = 8080
 RECEIVER_ADDR = (RECEIVER_IP, RECEIVER_PORT)
+TIMER_STOP = -1
+WINDOW_SIZE = 4
 
-packet_list = [];
-packet_lock = _thread.allocate_lock()
+# Shared resources across threads
+packets = []
+mutex = _thread.allocate_lock()
+base = 0
+timer_start = TIMER_STOP
 
-# Adds a packet to the end of packet_list
-def push_packet(packet):
-    global packet_list
-    global packet_lock
-    
-    packet_lock.acquire()
-    packet_list.append(packet)
-    packet_lock.release()
+def send(filename, sock):
+    global packets
+    global mutex
+    global base
+    global timer_start
 
-# Pops the packet from the front of packet_list
-def pop_packet():
-    global packet_list
-    global packet_lock
+    # TODO: Open file
+    # Add all the packets to the window
+    while True:
+        data = file.read(PACKET_SIZE)
+        if not data:
+            break
+        packets.append(data)
 
-    packet_lock.acquire()
-    if len(packet_list) == 0:
-        packet_lock.release()
-        return None
+    num_packets = len(packets)
+    next_to_send = 0
+    base = 0
 
-    return packet_list.pop(0)
-    packet_lock.release()
+    # Start the receiver thread
+    _thread.start_new_thread(receive, (sock,))
+
+    # Check the next to send and see if it's in the window
+    # If so, send it
+    # If no timer has started, start the timer for the packet
+    # While we cannot send any more, and there has been no timeout, sleep for a little bit
+
+def receive(sock):
+    global mutex
+    global base
+    global timer_start
+
+    while True:
+        data = socket.recvfrom(sock, RECEIVER_ADDR);
+        msg = data.decode('utf-8');
+        ack = int(msg);
+
+        # If we get an ACK for the first in-flight packet
+        if (ack == base):
+            mutex.acquire()
+            base += 1
+            timer_start = TIMER_STOP
+            mutex.release()
+
+        # Receive thread
+        # Once we receive an ACK for the send packet, increment the window base
+        # Stop the timer
 
 # Main function
 if __name__ == '__main__':
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.close()
