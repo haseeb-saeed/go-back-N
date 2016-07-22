749408696c3924cbe61405deb85925816d1a25fb
Haseeb Saeed <hs17@hotmail.co.uk>
2016-07-22 17:16:50 -0400

    lol forgot this

--- udt.py
+++ udt.py
@@ -2,10 +2,12 @@
 import random
 import socket
 
+DROP_PROB = 8
 
 # Send a packet across the unreliable channel
 # Packet may be lost
 def send(packet, sock, addr):
+    if random.randint(0, DROP_PROB) > 0:
         sock.sendto(packet, addr)
     return
 
